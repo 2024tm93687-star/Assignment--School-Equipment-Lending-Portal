0aaee290d314a2a037c9b1f3923a9a5040dcafeb
2024tm93684 <2024tm93684@wilp.bits-pilani.ac.in>
2025-10-26 16:10:39 -0400

    Get Equipment api

--- phase-1/server/equipment-service/src/routes/equipment.routes.js
+++ phase-1/server/equipment-service/src/routes/equipment.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import equipmentController from '../controllers/equipment.controller.js'
 
 const equipmentRouter = Router();
+equipmentRouter.get('/',equipmentController.getEquipment)
 equipmentRouter.post('/',equipmentController.createEquipment);
 equipmentRouter.put('/:id',equipmentController.updateEquipment);
 equipmentRouter.delete('/:id',equipmentController.deleteEquipment);
