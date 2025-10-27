f275ddfd1b96465565edee707f48cea7d4bb3141
Pradeep Eeda <2024tm93687@wilp.bits-pilani.ac.in>
2025-10-26 21:54:46 -0500

    add auth validation

--- phase-1/server/equipment-service/src/routes/equipment.routes.js
+++ phase-1/server/equipment-service/src/routes/equipment.routes.js
@@ -1,12 +1,12 @@
-import express from 'express';
 import { Router } from 'express';
-import equipmentController from '../controllers/equipment.controller.js'
+import equipmentController from '../controllers/equipment.controller.js';
+import authenticate from '../middleware/authentication.js';
 
 const equipmentRouter = Router();
-equipmentRouter.get('/',equipmentController.getEquipment)
-equipmentRouter.post('/',equipmentController.createEquipment);
-equipmentRouter.put('/:id',equipmentController.updateEquipment);
-equipmentRouter.delete('/:id',equipmentController.deleteEquipment);
+equipmentRouter.get('/', authenticate, equipmentController.getEquipment);
+equipmentRouter.post('/', authenticate, equipmentController.createEquipment);
+equipmentRouter.put('/:id', authenticate, equipmentController.updateEquipment);
+equipmentRouter.delete('/:id', authenticate, equipmentController.deleteEquipment);
 
 export default equipmentRouter;
 
